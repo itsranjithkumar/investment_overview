0a3a6cac78e30b3b7c726b2aabf7e5945685feeb
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2025-03-15 16:32:56 +0530

    changes

--- my-project/app/components/performance-chart.tsx
+++ my-project/app/components/performance-chart.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, Text } from "recharts"
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from "recharts"
 import { investmentData } from "../lib/data"
 
 export function PerformanceChart() {
